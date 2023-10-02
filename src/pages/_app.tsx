09ad28d897b09c9192b7e04fa4fe17c88814aa9f
Coin <cce5784@gmail.com>
2023-10-03 01:05:58 +0800

    在特定頁面隱藏底欄

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,21 +13,23 @@ const theme = createTheme({});
 export default function App({ Component, pageProps }: AppProps) {
   let [navbarTitle, setNavbarTitle] = useState('');
   let navbarTitleContextValue: NavbarTitleContextValue = [setNavbarTitle, navbarTitle];
+  let shouldHideBottomNav = !!(Component as any).hideBottomNav;
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <MantineProvider theme={theme}>
-      <NavbarTitleContext.Provider value={navbarTitleContextValue}>
-        <div className={styles.appShell}>
-          <TopNavbar />
-          <div className={styles.page}>
-            <Component {...pageProps} />
+        <NavbarTitleContext.Provider value={navbarTitleContextValue}>
+          <div className={styles.appShell}>
+            <TopNavbar />
+            <div className={styles.page}>
+              <Component {...pageProps} />
+            </div>
+            { !shouldHideBottomNav && <BottomNavbar /> }
           </div>
-          <BottomNavbar />
-        </div>
-      </NavbarTitleContext.Provider>
+        </NavbarTitleContext.Provider>
       </MantineProvider>
     </>
   );
