0108f6e6bcc7480e0ac8c2ee478f6fa49fb41b80
Coin <cce5784@gmail.com>
2023-10-11 21:39:05 +0800

    導覽列: 新增返回按鈕

--- src/components/app/TopNavbar.tsx
+++ src/components/app/TopNavbar.tsx
@@ -1,4 +1,7 @@
 import styles from '@/styles/app.module.css';
+import { UnstyledButton } from '@mantine/core';
+import { IconArrowLeft } from '@tabler/icons-react';
+import { useRouter } from 'next/router';
 import { createContext, useContext } from 'react';
 
 export type NavbarTitleContextValue = [(newTitle: string) => void, string];
@@ -8,12 +11,30 @@ export const NavbarTitleContext = createContext<NavbarTitleContextValue>([
   '',
 ]);
 
+const isBrowser = typeof window !== 'undefined';
+const initialHistoryLength = isBrowser ? window.history.length : 0;
+const topLevelNavDestinations = ['/events', '/forums', '/map', '/match', '/more'];
+
 export default function TopNavbar() {
   let [_, title] = useContext(NavbarTitleContext);
+  let router = useRouter();
+  let isTopLevelNavDestination = topLevelNavDestinations.includes(router.pathname);
+  let canGoBack = isBrowser ? window.history.length > initialHistoryLength : false;
+
+  function handleNavigateBack() {
+    if (canGoBack) router.back();
+    else router.push('/');
+  }
 
   return (
     <nav className={styles.topNav}>
-      <div>{/* 返回按鈕 */}</div>
+      <div className={styles.backButtonContainer}>
+        {!isTopLevelNavDestination && (
+          <UnstyledButton style={{ lineHeight: 1 }} px="sm" onClick={handleNavigateBack}>
+            <IconArrowLeft />
+          </UnstyledButton>
+        )}
+      </div>
       <div>{title}</div>
       <div>{/* placeholder */}</div>
     </nav>
