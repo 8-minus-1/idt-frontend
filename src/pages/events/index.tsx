188549c9efbb87f79acd3b601ecb772c26793a67
Tony <foxshih123@gmail.com>
2023-10-15 02:13:13 +0800

    更改頁面的雛形

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -41,24 +41,6 @@ import { createPortal } from 'react-dom';
 import { Simulate } from 'react-dom/test-utils';
 import submit = Simulate.submit;
 
-type ListContestPage = {
-  displayByc_id: (c_id: number) =>  void
-};
-
-type Contests = {
-  User_id: number,
-  Name: string,
-  Place: string,
-  Content: string,
-  sp_type: number,
-  StartDate: any,
-  EndDate: Date,
-  Deadline: string,
-  Url: string,
-  Other: string,
-  c_id: number
-}
-
 function AddContestPage({setFormToShow}: any)
 {
   const sports = [
@@ -206,11 +188,30 @@ function AddContestPage({setFormToShow}: any)
   )
 }
 
-function ListContestPage({displayByc_id}: ListContestPage)
+type ListContestPage = {
+  displayByc_id: (c_id: number) =>  void
+  setFormToShow: (FormType: number) => void
+};
+
+type Contests = {
+  User_id: number,
+  Name: string,
+  Place: string,
+  Content: string,
+  sp_type: number,
+  StartDate: any,
+  EndDate: Date,
+  Deadline: string,
+  Url: string,
+  Other: string,
+  c_id: number
+}
+
+function ListContestPage({displayByc_id, setFormToShow, setc_id}: ListContestPage)
 {
   const [sp_type, setSp_type] = useState<string|null>('0');
-
-  let { data, error } = useSWR(['cont/contests', { throwHttpErrors: true }]);
+  let str = (sp_type === '0') ? 'cont/contests' : 'cont/contests/SelectType?sp_type=' + sp_type;
+  let { data, error } = useSWR([str, { throwHttpErrors: true }]);
   if(data && data.length) console.log(data[0]);
   if(error) console.log("error: ",error);
 
@@ -233,21 +234,6 @@ function ListContestPage({displayByc_id}: ListContestPage)
     { value: "15", label: "其他" },
   ];
 
-  // data = [{
-  //   User_id: 1,
-  //   Name: "123",
-  //   Place: "string",
-  //   Constent: "string",
-  //   sp_type: 8,
-  //   StartDate: 2023-10-10,
-  //   EndDate: 2023-10-10,
-  //   Deadline: 2023-10-10,
-  //   Url: "string",
-  //   Other: "string",
-  //   c_id: 1}];
-  //   error = 0;
-  // console.log(data);
-
   return(
     <>
       <main>
@@ -261,17 +247,21 @@ function ListContestPage({displayByc_id}: ListContestPage)
             onChange={(value)=>(setSp_type(value))}
           />
           {data && data.map((contest: Contests) =>
-            // <Box>
-            //   <Text>{contest.Name}</Text>
-
-            // </Box>
             <Link href={"events/contests/"+contest.c_id} key={contest.c_id}>
               <Card padding="lg" pb='xl' bg="#D6EAF8" radius="lg" mb='xl' shadow='sm'>
                 <Group justify='space-between'>
                   <Group>
                     <IconUser />
-                    <Text fw={500}>User{contest.User_id}</Text>
+                    <Text fw={500} >User{contest.User_id}</Text>                     
                   </Group>
+
+                    <Flex mt='md' justify='right'>
+                      <Button fw={600} 
+                              style={{fontSize: '25px'}} 
+                              bg='#D6EAF8' 
+                              onClick={()=>(setFormToShow(3), displayByc_id(contest.c_id))}>...</Button>
+                    </Flex>
+
                 </Group>
                 <Text size="lg" m='md' fw='600'>{contest.Name}</Text>
                 <Text ml="xl" mr='lg' size='md' fw={500} lineClamp={3}>{contest.Content}</Text>
@@ -294,6 +284,154 @@ function ListContestPage({displayByc_id}: ListContestPage)
       </main>
     </>
   );
+}
+
+type ModifyContestPage ={
+  setContestByc_id: (c_id: number) =>  void
+}
+
+function ModifyContestPage({setContestByc_id}: ModifyContestPage)
+{
+  const [c_id, setc_id] = useState<number|null>(0);
+  let { data, error } = useSWR(['cont/contests/'+c_id, { throwHttpErrors: true }]);
+  if(error) console.log("error: ",error);
+  console.log(typeof(data));
+  
+  const sports = [
+    { value: "0", label: "顯示全部活動" },
+    { value: "1", label: "籃球" },
+    { value: "2", label: "排球" },
+    { value: "3", label: "羽球" },
+    { value: "4", label: "網球" },
+    { value: "5", label: "游泳" },
+    { value: "6", label: "直排輪" },
+    { value: "7", label: "足球" },
+    { value: "8", label: "桌球" },
+    { value: "9", label: "棒球" },
+    { value: "10", label: "壘球" },
+    { value: "11", label: "躲避球" },
+    { value: "12", label: "跆拳道" },
+    { value: "13", label: "巧固球" },
+    { value: "14", label: "保齡球" },
+    { value: "15", label: "其他" },
+  ];
+  
+  const form = useForm({
+    initialValues: {
+      Name: '',
+      Content:'',
+      Place:'',
+      sp_type:'',//前端送往後端時需處理
+      StartDate: new Date(),//前端送往後端時需處理
+      EndDate:'',//前端送往後端時需處理
+      Deadline:'',
+      Url:'',
+      Other:''
+    },
+  });
+    
+  form.setFieldValue('Name', data.Name);
+  form.setFieldValue('Content', data.Content);
+  form.setFieldValue('Place', data.Place);
+  form.setFieldValue('sp_type', data.sp_type);
+  form.setFieldValue('StartDate', data.StartDate);
+  form.setFieldValue('EndDate', data.EndDate);
+  form.setFieldValue('Deadline', data.Deadline);
+  form.setFieldValue('Url', data.Url);
+  form.setFieldValue('Other', data.Other);
+
+  return(
+    <>
+      <main>
+      <Box maw={500} mx="auto">
+        <form onSubmit={data}>
+          <Select
+            mt="md"
+            comboboxProps={{ withinPortal: true }}
+            label="運動類別"
+            data={sports}
+            {...form.getInputProps('sp_type')}
+          />
+          <TextInput
+            mt="md"
+            withAsterisk
+            label="活動名稱"
+            {...form.getInputProps('Name')}
+          />
+          <TextInput
+          mt="md"
+            withAsterisk
+            label="地點"
+            {...form.getInputProps('Place')}
+          />
+          <DateInput
+            mt="md"
+            withAsterisk
+            clearable defaultValue={new Date()}
+            dateParser={data.StartDate}
+            valueFormat="YYYY/MM/DD"
+            label="開始日期"
+            {...form.getInputProps('StartDate')}
+          />
+          <DateInput
+            mt="md"
+            withAsterisk
+            clearable defaultValue={new Date()}
+            dateParser={data.EndDate}
+            valueFormat="YYYY/MM/DD"
+            label="結束日期"
+            {...form.getInputProps('EndData')}
+          />
+          <DateInput
+            mt="md"
+            withAsterisk
+            clearable defaultValue={new Date()}
+            dateParser={data.Deadline}
+            valueFormat="YYYY/MM/DD"
+            label="截止日期"
+            {...form.getInputProps('Deadline')}
+          />
+          <TextInput
+            mt="md"
+            withAsterisk
+            label="內容"
+            {...form.getInputProps('Content')}
+          />
+          <TextInput
+            mt="md"
+            withAsterisk
+            label="報名連結"
+            {...form.getInputProps('Url')}
+          />
+          <TextInput
+            mt="md"
+            withAsterisk
+            label="其他"
+            {...form.getInputProps('Other')}
+          />
+        <Group justify={"space-evenly"} my={'lg'}>
+          <Button
+            onClick={()=>{window.location.href='/event'}}
+            leftSection={<IconChevronLeft />}
+            w={"45%"}
+          >
+            取消
+          </Button>
+          <Button
+            type={'submit'}
+            variant="gradient"
+            gradient={{ from: 'yellow', to: 'orange', deg: 90 }}
+            rightSection={<IconSend />}
+            w={"45%"} /*loading={loading}*/
+          >
+            送出
+          </Button>
+        </Group>
+        </form>
+          </Box>
+      </main>
+    </>
+  )
 }
 
 
@@ -331,6 +469,9 @@ export default function EventsPage() {
               displayByc_id = {(c_id) => {
                 setc_id(c_id);
               }}
+              setFormToShow = {(FormType) => {
+                  setFormToShow(FormType);
+              }}
             />
             { fabContainer && createPortal(
               <Button leftSection={<IconPlus size={20} />} mr={'xl'} mb='xl'
@@ -346,8 +487,9 @@ export default function EventsPage() {
           <AddContestPage setFormToShow={setFormToShow}></AddContestPage>
         )}
 
-        {formToShow === FormType.modifyContestForm}
-
+        {formToShow === FormType.modifyContestForm && (
+          <ModifyContestPage setContestByc_id={setc_id} ></ModifyContestPage>
+        )}
         {formToShow === FormType.unSignInForm && (
           <Box style={{
             textAlign: "center",
