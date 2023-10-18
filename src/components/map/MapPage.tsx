bca68da996b30766a2003d071f46d5c088b54796
Coin <cce5784@gmail.com>
2023-10-18 15:24:27 +0800

    地圖: 在網址裡儲存縮放跟平移資訊

--- src/components/map/MapPage.tsx
+++ src/components/map/MapPage.tsx
@@ -9,11 +9,15 @@ import {
   TileLayer,
   TileLayerProps,
   useMap,
+  useMapEvents,
 } from 'react-leaflet';
 import { Icon, Control } from 'leaflet';
 import { IconChevronRight } from '@tabler/icons-react';
 import { FABContainerContext } from '@/contexts/FABContainerContext';
 import { createPortal } from 'react-dom';
+import { useRouter } from 'next/router';
+import qs from 'querystring';
+
 import LeafletMarker from 'leaflet/dist/images/marker-icon.png';
 import LeafletMarker2x from 'leaflet/dist/images/marker-icon-2x.png';
 import LeafletMarkerShadow from 'leaflet/dist/images/marker-shadow.png';
@@ -63,9 +67,60 @@ function LeafletContainerStyle() {
   );
 }
 
-export default function MapPage() {
+function HashParamsLoader() {
+  const router = useRouter();
+  const map = useMap();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const url = new URL(location.href);
+
+    if (url.hash) {
+      const { z, x, y } = qs.parse(url.hash.slice(1), ';');
+      const zoom = Number(z);
+      const lng = Number(x);
+      const lat = Number(y);
+      if (Number.isNaN(lng) || Number.isNaN(lat) || Number.isNaN(zoom)) return;
+      if (zoom < map.getMinZoom() || zoom > map.getMaxZoom()) return;
+      map.setView([lat, lng], zoom);
+    }
+  }, [router.isReady]);
+
+  return <></>;
+}
+
+function MapPageInner() {
   let fabContainer = useContext(FABContainerContext);
   const [displayState, setDisplayState] = useState(0);
+  const map = useMap();
+  const router = useRouter();
+
+  function handleZoomAndCenterChange() {
+    if (!router.isReady) return;
+    const params = {
+      z: map.getZoom(),
+      x: map.getCenter().lng,
+      y: map.getCenter().lat,
+    };
+
+    router
+      .replace(
+        {
+          hash: qs.stringify(params, ';'),
+        },
+        undefined,
+        { shallow: true }
+      )
+      .catch((e) => {
+        if (!e.cancelled) throw e;
+      });
+  }
+
+  useMapEvents({
+    zoomend: handleZoomAndCenterChange,
+    moveend: handleZoomAndCenterChange,
+  });
+
   const places = [
     {
       ID: 1234,
@@ -83,56 +138,65 @@ export default function MapPage() {
 
   return (
     <>
+      {places.map(({ ID, Name, Latitude, Longitude }) => (
+        <Marker
+          key={ID}
+          position={{ lat: Latitude, lng: Longitude }}
+          eventHandlers={{
+            click: (e) => {
+              setDisplayState(1);
+            },
+          }}
+        >
+          <Popup>{Name}</Popup>
+        </Marker>
+      ))}
+      {displayState &&
+        fabContainer &&
+        createPortal(
+          <Card
+            w="100%"
+            padding="lg"
+            px="xl"
+            bg="#F2FAFC"
+            radius="lg"
+            mb="xl"
+            shadow="sm"
+            style={{ zIndex: 1, position: 'absolute', bottom: 0 }}
+          >
+            <Text fw="700" size="xl">
+              元智大學
+            </Text>
+            <Text>地址 : 320桃園市中壢區遠東路135號</Text>
+            <Text>聯繫方式 : 034638800</Text>
+            <Text>網站連結 : https://www.yzu.edu.tw/</Text>
+            <Text>更新時間 : 2023-10-18</Text>
+            <Flex c="blue" mt="md" justify="right">
+              <Text
+                style={{ textDecoration: 'underline', textDecorationThickness: rem(2) }}
+                fw={600}
+                size="md"
+              >
+                查看詳細內容
+              </Text>
+              <IconChevronRight />
+            </Flex>
+          </Card>,
+          fabContainer
+        )}
+    </>
+  );
+}
+
+export default function MapPage() {
+  return (
+    <>
       <LeafletContainerStyle />
       <MapContainer {...mapProps}>
+        <HashParamsLoader />
         <Resizer />
         <TileLayer {...tileLayerProps} />
-        {places.map(({ ID, Name, Latitude, Longitude }) => (
-          <Marker
-            key={ID}
-            position={{ lat: Latitude, lng: Longitude }}
-            eventHandlers={{
-              click: (e) => {
-                setDisplayState(1);
-              },
-            }}
-          >
-            <Popup>{Name}</Popup>
-          </Marker>
-        ))}
-        {displayState &&
-          fabContainer &&
-          createPortal(
-            <Card
-              w="100%"
-              padding="lg"
-              px="xl"
-              bg="#F2FAFC"
-              radius="lg"
-              mb="xl"
-              shadow="sm"
-              style={{ zIndex: 1, position: 'absolute', bottom: 0 }}
-            >
-              <Text fw="700" size="xl">
-                元智大學
-              </Text>
-              <Text>地址 : 320桃園市中壢區遠東路135號</Text>
-              <Text>聯繫方式 : 034638800</Text>
-              <Text>網站連結 : https://www.yzu.edu.tw/</Text>
-              <Text>更新時間 : 2023-10-18</Text>
-              <Flex c="blue" mt="md" justify="right">
-                <Text
-                  style={{ textDecoration: 'underline', textDecorationThickness: rem(2) }}
-                  fw={600}
-                  size="md"
-                >
-                  查看詳細內容
-                </Text>
-                <IconChevronRight />
-              </Flex>
-            </Card>,
-            fabContainer
-          )}
+        <MapPageInner />
       </MapContainer>
     </>
   );
