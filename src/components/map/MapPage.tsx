db2d00b31e77b7535208589bded286ca82baa0e9
Coin <cce5784@gmail.com>
2023-10-18 14:25:09 +0800

    format

--- src/components/map/MapPage.tsx
+++ src/components/map/MapPage.tsx
@@ -1,6 +1,6 @@
 import { useViewportSize } from '@mantine/hooks';
-import {Button, Card, Flex, Group, rem, Text} from '@mantine/core';
-import React, {useContext, useEffect, useState} from 'react';
+import { Card, Flex, rem, Text } from '@mantine/core';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   MapContainer,
   MapContainerProps,
@@ -11,17 +11,18 @@ import {
   useMap,
 } from 'react-leaflet';
 import { Icon, Control } from 'leaflet';
+import { IconChevronRight } from '@tabler/icons-react';
+import { FABContainerContext } from '@/contexts/FABContainerContext';
+import { createPortal } from 'react-dom';
 import LeafletMarker from 'leaflet/dist/images/marker-icon.png';
 import LeafletMarker2x from 'leaflet/dist/images/marker-icon-2x.png';
 import LeafletMarkerShadow from 'leaflet/dist/images/marker-shadow.png';
-import {IconChevronRight, IconEdit, IconUser} from "@tabler/icons-react";
+
 Icon.Default.prototype.options.iconUrl = LeafletMarker.src;
 Icon.Default.prototype.options.iconRetinaUrl = LeafletMarker2x.src;
 Icon.Default.prototype.options.shadowUrl = LeafletMarkerShadow.src;
 Icon.Default.prototype.options.shadowRetinaUrl = LeafletMarkerShadow.src;
 Control.Attribution.prototype.options.prefix = 'Leaflet';
-import { FABContainerContext } from '@/contexts/FABContainerContext';
-import { createPortal } from 'react-dom';
 
 const mapProps: MapContainerProps = {
   center: [24.97294, 121.25822],
@@ -63,7 +64,6 @@ function LeafletContainerStyle() {
 }
 
 export default function MapPage() {
-
   let fabContainer = useContext(FABContainerContext);
   const [displayState, setDisplayState] = useState(0);
   const places = [
@@ -83,7 +83,6 @@ export default function MapPage() {
 
   return (
     <>
-
       <LeafletContainerStyle />
       <MapContainer {...mapProps}>
         <Resizer />
@@ -94,25 +93,46 @@ export default function MapPage() {
             position={{ lat: Latitude, lng: Longitude }}
             eventHandlers={{
               click: (e) => {
-                setDisplayState(1) ;
+                setDisplayState(1);
               },
-          }}>
+            }}
+          >
             <Popup>{Name}</Popup>
           </Marker>
         ))}
-        { displayState && fabContainer && createPortal(
-          <Card w = {"100%"} padding="lg" px='xl' bg="#F2FAFC" radius="lg" mb='xl' shadow='sm' style={{zIndex: 1,position: "absolute",bottom: 0}}>
-            <Text fw='1200' size="xl">元智大學</Text>
-            <Text >地址 : 320桃園市中壢區遠東路135號</Text>
-            <Text >聯繫方式 : 034638800</Text>
-            <Text >網站連結 : https://www.yzu.edu.tw/</Text>
-            <Text >更新時間 : 2023-10-18</Text>
-            <Flex c={'blue'} mt='md' justify='right'>
-              <Text style={{textDecoration: "underline", textDecorationThickness: rem(2)}} fw={600} size='md'>查看詳細內容</Text>
-              <IconChevronRight/>
-            </Flex>
-
-          </Card>, fabContainer) }
+        {displayState &&
+          fabContainer &&
+          createPortal(
+            <Card
+              w="100%"
+              padding="lg"
+              px="xl"
+              bg="#F2FAFC"
+              radius="lg"
+              mb="xl"
+              shadow="sm"
+              style={{ zIndex: 1, position: 'absolute', bottom: 0 }}
+            >
+              <Text fw="700" size="xl">
+                元智大學
+              </Text>
+              <Text>地址 : 320桃園市中壢區遠東路135號</Text>
+              <Text>聯繫方式 : 034638800</Text>
+              <Text>網站連結 : https://www.yzu.edu.tw/</Text>
+              <Text>更新時間 : 2023-10-18</Text>
+              <Flex c="blue" mt="md" justify="right">
+                <Text
+                  style={{ textDecoration: 'underline', textDecorationThickness: rem(2) }}
+                  fw={600}
+                  size="md"
+                >
+                  查看詳細內容
+                </Text>
+                <IconChevronRight />
+              </Flex>
+            </Card>,
+            fabContainer
+          )}
       </MapContainer>
     </>
   );
